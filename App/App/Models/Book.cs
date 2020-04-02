d043af3b4b2ee74ce8be85507e60d1de70ec46ec
Kamil Wlazlo <kwlazlo10@gmail.com>
2020-04-02 21:35:54 +0200

    Book, libraries and location models

---
+++ App/App/Models/Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Models
+{
+    [Table("Books")]
+    public class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+
+        [Required]
+        [MaxLength(32, ErrorMessage = "Book title is to long (max 32 characters)")]
+        [MinLength(2, ErrorMessage = "Book title is to short (min 2 characters)")]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(32, ErrorMessage = "Name is to long (max 32 characters)")]
+        [MinLength(2, ErrorMessage = "Name is to short (min 2 characters)")]
+        public string AuthorName { get; set; }
+
+        [Required]
+        [MaxLength(32, ErrorMessage = "Surname is to long (max 32 characters)")]
+        [MinLength(2, ErrorMessage = "Surname is to short (min 2 characters)")]
+        public string AuthorSurname { get; set; }
+
+        [Required]
+        public Library Library { get; set; }
+
+        //TODO: Add User relation 1 user : many books
+
+        [NotMapped]
+        public string AuthorFullName { get => AuthorName + ' ' + AuthorSurname; }
+    }
+}
---
+++ App/App/Models/Library.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Models
+{
+    [Table("Libraries")]
+    public class Library
+    {
+        [Key]
+        public int LibraryId { get; set; }
+
+        [Required, MaxLength(32, ErrorMessage = "Name is to long (max 32 characters)")]
+        public string Name { get; set; }
+
+        [Required]
+        public Location Location { get; set; }
+    }
+}
