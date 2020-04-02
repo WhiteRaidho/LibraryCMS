7328f54f3b14a2f11db70123ccb64350f6fb7d90
Piotr Knapik <piotrekxp3@gmail.com>
2020-04-02 22:16:32 +0200

    Add Reviews

--- App/App/Models/AppUser.cs
+++ App/App/Models/AppUser.cs
@@ -13,20 +13,24 @@ namespace App.Models
         [Key]
         public string UserID { get; set; }
 
-        [MinLength(6, ErrorMessage = "User name too short"), MaxLength(32, ErrorMessage = "User name too long")]
+        [Required, MinLength(6, ErrorMessage = "User name too short"), MaxLength(32, ErrorMessage = "User name too long")]
         public string UserName { get; set; }
-        [MinLength(6, ErrorMessage = "Password too short"), MaxLength(32, ErrorMessage = "Password too long")]
+        [Required, MinLength(6, ErrorMessage = "Password too short"), MaxLength(32, ErrorMessage = "Password too long")]
         public string UserPassword { get; set; }
 
-        [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Email is not valid.")]
+        [Required, RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Email is not valid.")]
         public string Email { get; set; }
 
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
 
         //[DataType(DataType.Text)]
         //public DateTime BirthDate { get; set; }
 
         public string UserRoles { get; set; }
+
+        public ICollection<Review> Reviews { get; set; }
     }
 }
