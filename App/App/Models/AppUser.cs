1303b6e4bbd40877bc09c0b783971b02008fb52c
Piotr Knapik <piotrekxp3@gmail.com>
2020-04-02 22:27:47 +0200

    Remove redundant lines

--- App/App/Models/AppUser.cs
+++ App/App/Models/AppUser.cs
@@ -12,15 +12,12 @@ namespace App.Models
     {
         [Key]
         public string UserID { get; set; }
-
-        [MinLength(6, ErrorMessage = "User name too short"), MaxLength(32, ErrorMessage = "User name too long")]
+        
         [Required, MinLength(6, ErrorMessage = "User name too short"), MaxLength(32, ErrorMessage = "User name too long")]
         public string UserName { get; set; }
-        [MinLength(6, ErrorMessage = "Password too short"), MaxLength(32, ErrorMessage = "Password too long")]
         [Required, MinLength(6, ErrorMessage = "Password too short"), MaxLength(32, ErrorMessage = "Password too long")]
         public string UserPassword { get; set; }
-
-        [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Email is not valid.")]
+        
         [Required, RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Email is not valid.")]
         public string Email { get; set; }
 
