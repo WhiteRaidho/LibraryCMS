dbc2e335ee860ddca39406a731b8881238bd416d
Piotr Knapik <piotrekxp3@gmail.com>
2020-04-02 20:33:48 +0200

    Add User Model

---
+++ App/App/Controllers/AppUsersController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using App.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppUsersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AppUsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //GET: api/AppUsers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetAppUser()
+        {
+            return await _context.AppUser.ToListAsync();
+        }
+
+        //[HttpGet]
+        //public IEnumerable<AppUser> GetAppUsers()
+        //{
+        //    return _context.AppUser.ToList();
+        //}
+
+        // GET: api/AppUsers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetAppUser(string id)
+        {
+            var appUser = await _context.AppUser.FindAsync(id);
+
+            if (appUser == null)
+            {
+                return NotFound();
+            }
+
+            return appUser;
+        }
+
+        // PUT: api/AppUsers/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAppUser(string id, AppUser appUser)
+        {
+            if (id != appUser.UserID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(appUser).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AppUserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/AppUsers
+        [HttpPost]
+        public async Task<ActionResult<AppUser>> PostAppUser(AppUser appUser)
+        {
+            _context.AppUser.Add(appUser);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAppUser", new { id = appUser.UserID }, appUser);
+        }
+
+        // DELETE: api/AppUsers/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<AppUser>> DeleteAppUser(string id)
+        {
+            var appUser = await _context.AppUser.FindAsync(id);
+            if (appUser == null)
+            {
+                return NotFound();
+            }
+
+            _context.AppUser.Remove(appUser);
+            await _context.SaveChangesAsync();
+
+            return appUser;
+        }
+
+        private bool AppUserExists(string id)
+        {
+            return _context.AppUser.Any(e => e.UserID == id);
+        }
+    }
+}
---
+++ App/App/Models/AppUser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Models
+{
+    public class AppUser
+    {
+        [Key]
+        public string UserID { get; set; }
+
+        [MinLength(6, ErrorMessage = "User name too short"), MaxLength(32, ErrorMessage = "User name too long")]
+        public string UserName { get; set; }
+        [MinLength(6, ErrorMessage = "Password too short"), MaxLength(32, ErrorMessage = "Password too long")]
+        public string UserPassword { get; set; }
+
+        //[RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Email is not valid.")]
+        //public string Email { get; set; }
+
+        //public string FirstName { get; set; }
+        //public string LastName { get; set; }
+
+        //[DataType(DataType.Text)]
+        //public DateTime BirthDate { get; set; }
+    }
+}
