4c8384915d025263dce3381908b19e9aa287ef29
Kamil Wlazlo <kwlazlo10@gmail.com>
2020-04-02 19:11:47 +0200

    Db connection

---
+++ App/App/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //DbSet
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
