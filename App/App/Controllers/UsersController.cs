f966eaa06e623af40c382fcff6f76483ee3381fa
Piotr Knapik <piotrekxp3@gmail.com>
2020-05-06 00:02:29 +0200

    Change User controller for Services. Add basic UserService

--- App/App/Controllers/UsersController.cs
+++ App/App/Controllers/UsersController.cs
@@ -6,100 +6,114 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using App.Models;
+using App.Services;
+using Microsoft.AspNetCore.Authorization;
+using AutoMapper;
+using App.ViewModels;
 
 namespace App.Controllers
 {
-    [Route("api/[controller]")]
+    //[Authorize]
     [ApiController]
+    [Route("api/[controller]")]
     public class UsersController : ControllerBase
     {
-        private readonly ApplicationDbContext _context;
+        protected IMapper Mapper { get; }
+        protected UserService Users { get; }
 
-        public UsersController(ApplicationDbContext context)
+        public UsersController(IMapper mapper, UserService userService)
         {
-            _context = context;
+            Mapper = mapper;
+            Users = userService;
         }
 
-        // GET: api/Users
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        // GET: api/Users/me
+        [HttpGet("me")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<UserViewModel>> GetMe()
         {
-            return await _context.Users.ToListAsync();
+            var me = await Users.GetUserAsync("admin"); // TODO Get user by id from auth
+            if (me == null) return NotFound();
+
+            var result = Mapper.Map<UserViewModel>(me);
+            return (ActionResult<UserViewModel>)result;
         }
 
-        // GET: api/Users/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<User>> GetUser(string id)
-        {
-            var user = await _context.Users.FindAsync(id);
+        //// GET: api/Users/5
+        //[HttpGet("{id}")]
+        //public async Task<ActionResult<User>> GetUser(string id)
+        //{
+        //    var user = await _context.Users.FindAsync(id);
 
-            if (user == null)
-            {
-                return NotFound();
-            }
+        //    if (user == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return user;
-        }
+        //    return user;
+        //}
 
-        // PUT: api/Users/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutUser(string id, User user)
-        {
-            if (id != user.UserID)
-            {
-                return BadRequest();
-            }
+        //// PUT: api/Users/5
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> PutUser(string id, User user)
+        //{
+        //    if (id != user.UserID)
+        //    {
+        //        return BadRequest();
+        //    }
 
-            _context.Entry(user).State = EntityState.Modified;
+        //    _context.Entry(user).State = EntityState.Modified;
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!UserExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!UserExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
 
-            return NoContent();
-        }
+        //    return NoContent();
+        //}
 
-        // POST: api/Users
-        [HttpPost]
-        public async Task<ActionResult<User>> PostUser(User user)
-        {
-            _context.Users.Add(user);
-            await _context.SaveChangesAsync();
+        //// POST: api/Users
+        //[HttpPost]
+        //public async Task<ActionResult<User>> PostUser(User user)
+        //{
+        //    _context.Users.Add(user);
+        //    await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetUser", new { id = user.UserID }, user);
-        }
+        //    return CreatedAtAction("GetUser", new { id = user.UserID }, user);
+        //}
 
-        // DELETE: api/Users/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<User>> DeleteUser(string id)
-        {
-            var user = await _context.Users.FindAsync(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
+        //// DELETE: api/Users/5
+        //[HttpDelete("{id}")]
+        //public async Task<ActionResult<User>> DeleteUser(string id)
+        //{
+        //    var user = await _context.Users.FindAsync(id);
+        //    if (user == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            _context.Users.Remove(user);
-            await _context.SaveChangesAsync();
+        //    _context.Users.Remove(user);
+        //    await _context.SaveChangesAsync();
 
-            return user;
-        }
+        //    return user;
+        //}
 
-        private bool UserExists(string id)
-        {
-            return _context.Users.Any(e => e.UserID == id);
-        }
+        //private bool UserExists(string id)
+        //{
+        //    return _context.Users.Any(e => e.UserID == id);
+        //}
     }
 }
