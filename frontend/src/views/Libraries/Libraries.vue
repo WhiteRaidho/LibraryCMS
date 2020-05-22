d7588b5dff89e23c808d152f9f588fcdb90d5247
Kamil Wlazlo <kwlazlo10@gmail.com>
2020-05-22 13:04:43 +0200

    [F] Cleaning unecessary stuff from Libraries viw

--- frontend/src/views/Libraries/Libraries.vue
+++ frontend/src/views/Libraries/Libraries.vue
@@ -40,37 +40,3 @@ export default class Libraries extends Vue {
   }
 }
 </script>
-
-<style lang="scss">
-.content-table {
-  border-collapse: collapse;
-  margin: auto;
-  margin-top: 20px;
-  font-size: 0.9em;
-  min-width: 700px;
-  border-radius: 8px 8px 0 0;
-  overflow: hidden;
-  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
-  padding: 20px;
-}
-.content-table thead tr {
-  background-color: var(--info-color);
-  color: var(--main-white-color);
-  text-align: left;
-  font-weight: bold;
-}
-.content-table th,
-.content-table td {
-  padding: 12px 15px;
-  text-align: left;
-}
-.content-table tbody tr {
-  border-bottom: 1px solid #dddddd;
-}
-.content-table tbody tr:nth-of-type(even) {
-  background-color: #f3f3f340;
-}
-.content-table tbody tr:last-of-type {
-  border-bottom: 2px solid var(--info-color);
-}
-</style>
