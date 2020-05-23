fd37d9f529993aed761b7cec08feccc9c6ca4394
Piotr Knapik <piotrekxp3@gmail.com>
2020-05-24 01:45:04 +0200

    [F] Add error handling for axios responses

--- frontend/src/controls/axios.ts
+++ frontend/src/controls/axios.ts
@@ -8,3 +8,27 @@ Vue.axios.interceptors.request.use(config => {
     // let url = new URL(`${config.baseURL}/${config.url}`);
     return config;
 });
+
+Vue.axios.interceptors.response.use(
+    response => {
+        return response;
+    },
+    error => {
+        const ex = {
+            code: 404,
+            message: "Not found",
+            data: null as any,
+            inner: null as any
+        };
+
+        if(error.response)
+        {
+            ex.code = error.response.status;
+            ex.data = (typeof error.response.data === "string")?{message: error.response.data}:error.response.data;
+            ex.message = ex.data.message?ex.data.message:error.message;
+            ex.inner = error;
+        }
+
+        return Promise.reject(ex);
+    }
+);
