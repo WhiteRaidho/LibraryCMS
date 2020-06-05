7a9a13dd2bfe986e8c94f02f518e85689197eea0
Kamil Wlazlo <kwlazlo10@gmail.com>
2020-06-05 20:11:24 +0200

    [B] Library CRUD

---
+++ App/App/ViewModels/LibraryFormModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App.ViewModels
+{
+    public class LibraryFormModel
+    {
+        [Required(ErrorMessage = "Id jest wymagane")]
+        public int LibraryId { get; set; }
+        [Required]
+        [MaxLength(32, ErrorMessage = "Nazwa może zawierać maksymalnie 32 znaki")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Miejscowość jest wymagana")]
+        public int LocationId { get; set; }
+    }
+}
