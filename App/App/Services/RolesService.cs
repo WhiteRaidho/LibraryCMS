58859980ecc1994a6919e1048d896024d1ffaf95
Kamil Wlazlo <kwlazlo10@gmail.com>
2020-06-06 00:14:08 +0200

    [B] GetUserRoles method

--- App/App/Services/RolesService.cs
+++ App/App/Services/RolesService.cs
@@ -2,6 +2,7 @@
 using App.ViewModels;
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace App.Services
@@ -30,6 +31,28 @@ namespace App.Services
             SaveChanges();
 
             return entity;
+        }
+
+        public List<Role> GetUserRoles(string userId)
+        {
+            var result = Context.Roles
+                .Include(u => u.User)
+                .Include(l => l.Library)
+                .Where(x => x.User.UserID == userId)
+                .ToList();
+
+            return result;
+        }
+
+        public List<Role> GetUserRoles(string userId, int libraryId)
+        {
+            var result = Context.Roles
+                .Include(u => u.User)
+                .Include(l => l.Library)
+                .Where(x => x.User.UserID == userId && x.Library.LibraryId == libraryId)
+                .ToList();
+
+            return result;
         }
     }
 }
